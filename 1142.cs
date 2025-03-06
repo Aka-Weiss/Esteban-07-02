1713c1e1e52e54e419bdb4dc2f112376136fdcc1
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 20:07:01 -0300

    Update 1142.cs

--- 1142.cs
+++ 1142.cs
@@ -3,7 +3,8 @@ using System;
 class URI {
     static void Main(string[] args) {
         int N = int.Parse(Console.ReadLine());
-        for(int i = 1; i < 4 * N; i += 4){
+
+        for (int i = 1; i < 4 * N; i += 4) {
             Console.WriteLine($"{i} {i + 1} {i + 2} PUM");
         }
     }
