77e0b55d92bd14c68805f8ce01d5b63965e896ad
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 20:06:31 -0300

    Update 1132.cs

--- 1132.cs
+++ 1132.cs
@@ -5,20 +5,15 @@ class URI {
         int X = int.Parse(Console.ReadLine());
         int Y = int.Parse(Console.ReadLine());
 
-        if(X > Y){
+        if (X > Y) {
             int temp = X;
             X = Y;
             Y = temp;
         }
 
         int soma = 0;
-        for(int i = X; i <= Y; ++i){
-            if(i % 13 == 0){
+        for (int i = X; i <= Y; ++i) {
+            if (i % 13 == 0) {
                 continue;
             }
             soma += i;
-        }
-
-        Console.WriteLine(soma);
-    }
-}
