bca6a45a857ff0fa60d95f56f28f08f7658b0456
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 20:00:17 -0300

    Update 1073.cs

--- 1073.cs
+++ 1073.cs
@@ -1,12 +1,15 @@
 using System;
 
-class URI {
-    static void Main(string[] args) {
-        int N;
-        
-        N = int.Parse(Console.ReadLine());
-        
-        for(int i = 2; i <= N; i += 2){
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int limite;
+
+        limite = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= limite; i += 2)
+        {
             Console.WriteLine($"{i}^2 = {i * i}");
         }
     }
