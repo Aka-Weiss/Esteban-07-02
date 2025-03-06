e93ae17a4de9037bfe80d19578e969954b5ec802
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 20:05:15 -0300

    Update 1097.cs

--- 1097.cs
+++ 1097.cs
@@ -2,12 +2,13 @@ using System;
 
 class URI {
     static void Main(string[] args) {
-        int k = 7;
-        for(int i = 1; i < 10; i += 2){
-            for(int j = k; j > k - 3; --j){
+        int valorJ = 7;
+
+        for (int i = 1; i < 10; i += 2) {
+            for (int j = valorJ; j > valorJ - 3; --j) {
                 Console.WriteLine($"I={i} J={j}");
             }
-            k += 2;
+            valorJ += 2;
         }
     }
 }
