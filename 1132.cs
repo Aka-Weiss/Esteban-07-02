7aa71027a10a0ec3c936c3fe32217dbde63476fe
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 19:53:38 -0300

    Update 1132.cs

--- 1132.cs
+++ 1132.cs
@@ -1,39 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Exercicio1071
-{
-    class Program
-    {
-        static void Main()
-        {
-            int x, y, soma = 0;
+class URI {
+    static void Main(string[] args) {
+        int X = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite dois números inteiros: ");
-            x = int.Parse(Console.ReadLine());
-            y = int.Parse(Console.ReadLine());
-            if (x > y)
-            {
-                int newX;
-                newX = y;
-                y = x;
-                x = newX;
-            }
-
-            for (int i = x; i <= y; i++)
-            {
-                if (i % 13 != 0)
-                {
-                    soma += i;
-
-                }
-
-            }
-            Console.WriteLine($"Soma dos números entre {x} e {y} (incluindo os mesmos) que não são divisíveis por 13 é igual a: {soma}");
-            Console.ReadKey();
+        if(X > Y){
+            int temp = X;
+            X = Y;
+            Y = temp;
         }
+
+        int soma = 0;
+        for(int i = X; i <= Y; ++i){
+            if(i % 13 == 0){
+                continue;
+            }
+            soma += i;
+        }
+
+        Console.WriteLine(soma);
     }
 }
