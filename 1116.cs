b81fc7999fac3f14e454e9b04f263d27b3a0c610
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 20:05:54 -0300

    Update 1116.cs

--- 1116.cs
+++ 1116.cs
@@ -6,13 +6,14 @@ class URI {
     static void Main(string[] args) {
         int N = int.Parse(Console.ReadLine());
 
-        for(int i = 0; i < N; ++i){
-            List<int> entrada = Console.ReadLine().Trim().Split(' ').Select((x) => int.Parse(
+        for (int i = 0; i < N; ++i) {
+            List<int> entrada = Console.ReadLine().Trim().Split(' ').Select(x => int.Parse(x)).ToList();
 
-            if(entrada[1] == 0){
+            if (entrada[1] == 0) {
                 Console.WriteLine("divisao impossivel");
-            }else{
-                Console.WriteLine($"{(double)entrada[0]/entrada[1]:0.0}");
+            } else {
+                Console.WriteLine($"{(double)entrada[0] / entrada[1]:0.0}");
             }
         }
     }
+}
