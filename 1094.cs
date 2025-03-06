deb29b8dda3d0db8d0a7783a6e2314ced04adcf7
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 20:02:50 -0300

    Update 1094.cs

--- 1094.cs
+++ 1094.cs
@@ -1,34 +1,43 @@
 using System;
 
-class URI {
-    static void Main(string[] args) {
-        int N = int.Parse(Console.ReadLine());
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int numeroDeTestes = int.Parse(Console.ReadLine());
 
-        int coelhos = 0;
-        int ratos = 0;
-        int sapos = 0;
-        for(int i = 0; i < N; ++i){
+        int totalCoelhos = 0;
+        int totalRatos = 0;
+        int totalSapos = 0;
+
+        for (int i = 0; i < numeroDeTestes; ++i)
+        {
             string[] entrada = Console.ReadLine().Trim().Split(' ');
-            int Quantia = int.Parse(entrada[0]);
-            string Tipo = entrada[1];
+            int quantidade = int.Parse(entrada[0]);
+            string tipoDeCobaia = entrada[1];
 
-            switch(Tipo){
-                case "C":   coelhos += Quantia;
-                            break;
-                case "R":   ratos += Quantia;
-                            break;
-                case "S":   sapos += Quantia;
-                            break;
+            switch (tipoDeCobaia)
+            {
+                case "C":
+                    totalCoelhos += quantidade;
+                    break;
+                case "R":
+                    totalRatos += quantidade;
+                    break;
+                case "S":
+                    totalSapos += quantidade;
+                    break;
             }
         }
-        int total = coelhos + ratos + sapos;
 
-        Console.WriteLine($"Total: {total} cobaias");
-        Console.WriteLine($"Total de coelhos: {coelhos}");
-        Console.WriteLine($"Total de ratos: {ratos}");
-        Console.WriteLine($"Total de sapos: {sapos}");
-        Console.WriteLine($"Percentual de coelhos: {(double)coelhos/total * 100:0.00} %");
-        Console.WriteLine($"Percentual de ratos: {(double)ratos/total * 100:0.00} %");
-        Console.WriteLine($"Percentual de sapos: {(double)sapos/total * 100:0.00} %");
+        int totalCobaias = totalCoelhos + totalRatos + totalSapos;
+
+        Console.WriteLine($"Total: {totalCobaias} cobaias");
+        Console.WriteLine($"Total de coelhos: {totalCoelhos}");
+        Console.WriteLine($"Total de ratos: {totalRatos}");
+        Console.WriteLine($"Total de sapos: {totalSapos}");
+        Console.WriteLine($"Percentual de coelhos: {(double)totalCoelhos / totalCobaias * 100:0.00} %");
+        Console.WriteLine($"Percentual de ratos: {(double)totalRatos / totalCobaias * 100:0.00} %");
+        Console.WriteLine($"Percentual de sapos: {(double)totalSapos / totalCobaias * 100:0.00} %");
     }
 }
