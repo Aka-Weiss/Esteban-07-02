2c41f962ef9b4a6ebdf073e2fe857e4d26bf180f
Aka-Weiss <Onlyweiss@proton.me>
2025-03-06 20:01:51 -0300

    Update 1080.cs

--- 1080.cs
+++ 1080.cs
@@ -1,19 +1,23 @@
 using System;
 
-class URI {
-    static void Main(string[] args) {
-        int numero, maior = int.MinValue, posicao = 1;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int numero, maiorNumero = int.MinValue, posicaoMaiorNumero = 1;
 
-        for(int i = 0; i < 100; ++i){
+        for (int i = 0; i < 100; ++i)
+        {
             numero = int.Parse(Console.ReadLine());
 
-            if(numero > maior){
-                maior = numero;
-                posicao = i + 1;
+            if (numero > maiorNumero)
+            {
+                maiorNumero = numero;
+                posicaoMaiorNumero = i + 1;
             }
         }
 
-        Console.WriteLine(maior);
-        Console.WriteLine(posicao);
+        Console.WriteLine(maiorNumero);
+        Console.WriteLine(posicaoMaiorNumero);
     }
 }
